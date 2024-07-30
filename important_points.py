cf3af0c37a0bd1ae6deee638c71b1d371deefcc7
yash-op7 <yashm3375@gmail.com>
2024-07-30 07:18:14 +0530

    noting about the __repr__() and __eq__() methods

---
+++ important_points.py
@@ -0,0 +1,23 @@
+
+#! 1. __repr__ and __eq__ methods
+#! inside python ooops you can override certain functions with a predefined name for a predefined functionality
+# for example
+
+class Point:
+    x:int
+    y:int
+
+    def __init__(self, x, y):
+        self.x=x
+        self.y=y
+    
+    def __repr__(self) -> str:
+        return f'Point(x={self.x}, y={self.y})'
+    
+    def __eq__(self, other: object) -> bool:
+        return self.x == other.x and self.y == other.y
+    
+p1 = Point(1, 2)
+p2 = Point(3, 3)
+print(p1)
+print(p1 == p2)
