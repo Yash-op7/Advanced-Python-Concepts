8dc42e296500de7a0a8f0c8066056640f2ddf744
yash-op7 <yashm3375@gmail.com>
2024-07-30 06:58:51 +0530

    finished python functions

--- important_python_functions.py
+++ important_python_functions.py
@@ -107,6 +107,17 @@ genders = ['female', 'male', 'male']
 
 # this is how to properly use it
 
-file = open("test.txt", 'w')    #! there are many options, but the important ones are 'w' - write, 'r' - read, 'a' = append
-#! when you use 'w' it will overwrite a file if it already exists, essentially clear everything inside the file, so be careful with this
-file.write("hello\ni wanna die :)")
+# file = open("test.txt", 'w')    #! there are many options, but the important ones are 'w' - write, 'r' - read, 'a' = append, 'wr' = read and write and so on
+
+# #! when you use 'w' it will overwrite a file if it already exists, essentially clear everything inside the file, so be careful with this
+# file.write("hello\ni wanna die :)")
+# file.close() # to make sure there aren't any memory leaks
+
+#! you should just use the 'with' syntax instead of manually opening and closing
+
+# with open("test.txt", 'w') as file:     # use a context manager
+#     file.write("ayanogod")
+
+with open("test.txt", 'r') as file:
+    text = file.read()
+    print(text)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,8 +1,4 @@
-
-#     """
-#     a: number of cats
-#     b: a dictionary pertaining to the properties of a cat
-
-#     returns: nothign, just an example function to print some random stuff
-#     """
-#     print(a, kwargs['cat'])
+w')    #! there are many options, but the important ones are 'w' - write, 'r' - read, 'a' = append
+# #! when you use 'w' it will overwrite a file if it already exists, essentially clear everything inside the file, so be careful with this
+# file.write("hello\ni wanna die :)")
+# file.close() # to make sure there
