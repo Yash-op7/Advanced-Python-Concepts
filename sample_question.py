47e1a0417f5ede81a88b8183f8c01893845036da
yash-op7 <yashm3375@gmail.com>
2024-09-04 12:24:06 +0530

    solved the sample quesiton

---
+++ sample_question.py
@@ -0,0 +1,26 @@
+def count_email_domains(emails: list[str], urls: list[str]) -> dict[str, int]:
+    """
+    Given a list of emails and URLs, returns a dictionary of count of emails per URL domain.
+    :param emails: list[str]
+    :param urls: list[str]
+    :return emails_per_url_domain: dict[str, int]
+    """
+    emails_per_url_domain = {url:0 for url in urls}
+    
+    for email in emails:
+        if email.count('@') != 1:
+            continue
+        domain = email.partition('@')[2]
+        if (url := 'www.' + domain) in emails_per_url_domain:
+            emails_per_url_domain[url] += 1
+    
+    return emails_per_url_domain
+
+def main() -> None:
+    emails = ['foo@a.com','bar@a.com' , 'baz@b.com', 'qux@d.com']
+    urls = ['www.a.com', 'www.b.com', 'www.c.com']
+    emails_per_url_domain = count_email_domains(emails=emails, urls=urls)
+    print(f'{emails_per_url_domain = }')
+
+if __name__=='__main__':
+    main()
