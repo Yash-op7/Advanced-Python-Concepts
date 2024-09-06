1dcc465285f37dd3b6f7062a403d9b79a9ddd70f
yash-op7 <yashm3375@gmail.com>
2024-09-06 17:38:25 +0530

    clarified assertions

--- test.py
+++ test.py
@@ -1,13 +1,19 @@
 
 def is_input_valid(ops: list[str]) -> bool:
+    expected_values: set[str] = {'+', 'D', 'C'}
+    for value in ops:
+        if value in expected_values:
+            continue
+        if not value.isdecimal():
+            return False
     return True
 
 def perfCalOps(ops: list[str]) -> int:
     return 10
 
 def main() -> None:
-    ops: list[str] = ['24', '+', 'D', 'C', '4', '51']
-    assert is_input_valid(ops)
+    ops: list[str] = ['24', '+', "D", 'C', '4', '51']
+    assert is_input_valid(ops), 'Invalid input'
     print(perfCalOps(ops))
 
 if __name__ == '__main__':
