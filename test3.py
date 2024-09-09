83af6af2d6bd342c97fadaf703dd60a822128594
yash-op7 <yashm3375@gmail.com>
2024-09-09 18:22:17 +0530

    done for now

--- test3.py
+++ test3.py
@@ -1,7 +1,22 @@
-def main() -> None:
-    a, (b, c) = ['bird', (1, 2)]
-    print(a, b, c)
+s = 'abc!, vbd, sdaoj, ,ijf ds   fad'
+gen_obj = (clip for clip in s.split(sep=' ') if clip)
 
+def check(clip):
+    # Membership checks
+    for ch in clip:
+        if ch.isdigit():
+            return False
+        elif ch.isalpha():
+            if ch.lower() != ch:
+                return False
+        else:
+            if (ch != '-') or ()
+    # dash check
+    # special symbol check
 
+cnt = 0
+for clip in gen_obj:
+    if check(clip):
+        cnt += 1
 
-main()
+print(cnt)
